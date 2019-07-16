df701a2c078dc589ca132ccb7b1aa37034f70b39
Markus Rudel <markus.rudel@ruhr-uni-bochum.de>
2019-07-16 11:45:18 +0200

    removed leftovers from ColorTemperature example

--- Schalterbox/Schalterbox.ino
+++ Schalterbox/Schalterbox.ino
@@ -37,44 +37,6 @@ CRGB leds[NUM_LEDS];
 
 #define BRIGHTNESS  128
 
-
-// FastLED v2.1 provides two color-management controls:
-//   (1) color correction settings for each LED strip, and
-//   (2) master control of the overall output 'color temperature' 
-//
-// THIS EXAMPLE demonstrates the second, "color temperature" control.
-// It shows a simple rainbow animation first with one temperature profile,
-// and a few seconds later, with a different temperature profile.
-//
-// The first pixel of the strip will show the color temperature.
-//
-// HELPFUL HINTS for "seeing" the effect in this demo:
-// * Don't look directly at the LED pixels.  Shine the LEDs aganst
-//   a white wall, table, or piece of paper, and look at the reflected light.
-//
-// * If you watch it for a bit, and then walk away, and then come back 
-//   to it, you'll probably be able to "see" whether it's currently using
-//   the 'redder' or the 'bluer' temperature profile, even not counting
-//   the lowest 'indicator' pixel.
-//
-//
-// FastLED provides these pre-conigured incandescent color profiles:
-//     Candle, Tungsten40W, Tungsten100W, Halogen, CarbonArc,
-//     HighNoonSun, DirectSunlight, OvercastSky, ClearBlueSky,
-// FastLED provides these pre-configured gaseous-light color profiles:
-//     WarmFluorescent, StandardFluorescent, CoolWhiteFluorescent,
-//     FullSpectrumFluorescent, GrowLightFluorescent, BlackLightFluorescent,
-//     MercuryVapor, SodiumVapor, MetalHalide, HighPressureSodium,
-// FastLED also provides an "Uncorrected temperature" profile
-//    UncorrectedTemperature;
-
-#define TEMPERATURE_1 Tungsten100W
-
-// How many seconds to show each temperature before switching
-#define DISPLAYTIME 20
-// How many seconds to show black between switches
-#define BLACKTIME   3
-
 int internalLedState = LOW;
 int activeLedNumber = 0;
 int ledInc = 1;
@@ -142,10 +104,6 @@ void playBeep() {
 
 void loop()
 {
-    uint8_t secs = (millis() / 1000) % (DISPLAYTIME * 2);
-    FastLED.setTemperature( TEMPERATURE_1 ); // first temperature
-    leds[0] = TEMPERATURE_1; // show indicator pixel
-
     FastLED.clear();
     for (int i = 0; i < NUM_LEDS; i++) {
         if (i == activeLedNumber) {
