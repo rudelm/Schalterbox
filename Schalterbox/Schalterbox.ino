8b5f1c5f5b196173bbcbbf648f2211b04353ecdb
Markus Rudel <markus.rudel@ruhr-uni-bochum.de>
2020-06-20 22:55:04 +0200

    first version based on one_sine_pal

--- Schalterbox/Schalterbox.ino
+++ Schalterbox/Schalterbox.ino
@@ -1,7 +1,28 @@
+/*
+ * Contains code from one_sine_pal by Andrew Tuline
+ * https://github.com/atuline/FastLED-Demos/blob/master/one_sine_pal/one_sine_pal.ino
+ * 
+ * Forget using delay() statements or counting pixels up and down the strand of LED's. This example demonstrates just a bit of what can be done using basic high school math
+ * by using a simple sinewave.
+ * 
+ * You can use a sine wave to go back and forth up a strand of LED's. You can shift the phase of a sine wave to move along a strand. You can clip the top of that sine wave
+ * to just display a certain value (or greater). You can add palettes to that sine wave so that you aren't stuck with CHSV values and so on.
+ * 
+ * It shows that you don't neeed a pile of for loops, delays or counting pixels in order to come up with a decent animation.
+ */
+
 #include <FastLED.h>
 #include <Rotary.h>
 #include <Bounce2.h>
 #include "pitches.h"
+
+// Use qsuba for smooth pixel colouring and qsubd for non-smooth pixel colouring
+#define qsubd(x, b)  ((x>b)?b:0)                            // Digital unsigned subtraction macro. if result <0, then => 0. Otherwise, take on fixed value.
+#define qsuba(x, b)  ((x>b)?x-b:0)                          // Analog Unsigned subtraction macro. if result <0, then => 0
+
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
 
 #define ENCODER_PIN_BUTTON 2
 #define ENCODER_PIN_A 3
@@ -10,7 +31,6 @@
 #define BUZZER_SWITCH_PIN 6
 #define DATA_PIN      11
 #define CLOCK_PIN     12
-#define LED_PIN       13 // Its the internal LED on the Arduino
 #define BUTTON_RED_PIN 22
 #define BUTTON_GREEN_PIN 24
 #define BUTTON_BLUE_PIN 26
@@ -40,9 +60,23 @@ const uint8_t BUTTON_PINS[NUM_BUTTONS] = {ENCODER_PIN_BUTTON, BUTTON_RED_PIN, BU
 #define NUM_LEDS    12
 #define CHIPSET     WS2801
 #define COLOR_ORDER RGB
-CRGB leds[NUM_LEDS];
+struct CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS  128
+uint8_t max_bright = 128;                                     // Overall brightness definition. It can be changed on the fly.
+
+// Initialize changeable global variables. Play around with these!!!
+int8_t thisspeed = 8;                                         // You can change the speed of the wave, and use negative values.
+uint8_t allfreq = 32;                                         // You can change the frequency, thus distance between bars.
+int thisphase = 0;                                            // Phase change value gets calculated.
+uint8_t thiscutoff = 192;                                     // You can change the cutoff value to display this wave. Lower value = longer wave.
+int thisdelay = 30;                                           // You can change the delay. Also you can change the allspeed variable above. 
+uint8_t bgclr = 0;                                            // A rotating background colour.
+uint8_t bgbright = 10;                                        // Brightness of background colour
+
+// Palette definitions
+CRGBPalette16 currentPalette;
+CRGBPalette16 targetPalette;
+TBlendType    currentBlending;
 
 int internalLedState = LOW;
 int activeLedNumber = 0;
@@ -86,20 +120,34 @@ void setup() {
         buzzerEnabled = false;
     }
     
-    pinMode(LED_PIN,OUTPUT); // Setup the LED
-    digitalWrite(LED_PIN,internalLedState);
     playMelody();
 
-    delay(3000); // power-up safety delay
     delay(1000); // power-up safety delay
     // It's important to set the color correction for your LED strip here,
     // so that colors can be more accurately rendered through the 'temperature' profiles
     FastLED.addLeds<CHIPSET, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS)
-        .setCorrection( Typical8mmPixel )
-        .setDither(BRIGHTNESS < 255);
-    FastLED.setBrightness( BRIGHTNESS );
+        .setCorrection( Typical8mmPixel );
+    FastLED.setBrightness(max_bright);
     FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);               // FastLED Power management set at 5V, 500mA.
+
+    currentPalette = LavaColors_p;
+    currentBlending = LINEARBLEND;
 }
+
+void one_sine_pal(uint8_t colorIndex) {                                       // This is the heart of this program. Sure is short.
+  
+  thisphase += thisspeed;                                                     // You can change direction and speed individually.
+  
+  for (int k=0; k<=NUM_LEDS-1; k++) {                                          // For each of the LED's in the strand, set a brightness based on a wave as follows:
+    int thisbright = qsubd(cubicwave8((k*allfreq)+thisphase), thiscutoff);    // qsub sets a minimum value called thiscutoff. If < thiscutoff, then bright = 0. Otherwise, bright = 128 (as defined in qsub)..
+    leds[k] = CHSV(bgclr, 255, bgbright);                                     // First set a background colour, but fully saturated.
+    leds[k] += ColorFromPalette( currentPalette, colorIndex, thisbright, currentBlending);    // Let's now add the foreground colour.
+    colorIndex +=3;
+  }
+  
+  bgclr++;
+  
+} // one_sine_pal()
 
 void playMelody() {
     if (buzzerEnabled) {
@@ -210,30 +258,22 @@ void processClickWheelInputs() {
 
 void loop()
 {
-    FastLED.clear();
-    for (int i = 0; i < NUM_LEDS; i++) {
-        if (i == activeLedNumber) {
-            leds[i] = CRGB::Green;
-        } else {
-            leds[i] = CRGB::Blue;
-        }
+    processButtonInputs();
+    processClickWheelInputs();  
+
+    EVERY_N_MILLISECONDS(thisdelay) {                           // FastLED based non-blocking delay to update/display the sequence.
+        one_sine_pal(millis()>>4);
+    }
+
+    EVERY_N_MILLISECONDS(100) {
+        uint8_t maxChanges = 24; 
+        nblendPaletteTowardPalette(currentPalette, targetPalette, maxChanges);   // AWESOME palette blending capability.
+    }
+
+    EVERY_N_SECONDS(5) {                                        // Change the target palette to a random one every 5 seconds.
+        static uint8_t baseC = random8();                         // You can use this as a baseline colour if you want similar hues in the next line.
+        targetPalette = CRGBPalette16(CHSV(random8(), 255, random8(128,255)), CHSV(random8(), 255, random8(128,255)), CHSV(random8(), 192, random8(128,255)), CHSV(random8(), 255, random8(128,255)));
     }
     
     FastLED.show();
-    
-    for (int t = 0; t < NUM_LEDS; t++) {
-        FastLED.delay(ledSpeed);
-
-        processButtonInputs();
-        processClickWheelInputs();
-
-        // if a LED toggle has been flagged :
-        if (needToToggleLed) {
-            // Toggle LED state :
-            internalLedState = !internalLedState;
-            digitalWrite(LED_PIN, internalLedState);
-        }
-    }
-
-    activeLedNumber = (activeLedNumber + ledInc) % NUM_LEDS;
 }
