8e6d58d38f97c141e86673ffef3d9d0d8c61b7a8
Markus Rudel <markus.rudel@ruhr-uni-bochum.de>
2020-06-23 22:58:32 +0200

    Change colors based on pressed button and wheel

--- Schalterbox/Schalterbox.ino
+++ Schalterbox/Schalterbox.ino
@@ -50,6 +50,9 @@ uint8_t max_bright = 128;                                     // Overall brightn
 
 int internalLedState = LOW;
 int activeLedNumber = 0;
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
 
 // notes in the melody:
 int melody[] = {
@@ -62,6 +65,9 @@ int noteDurations[] = {
 };
 
 boolean buzzerEnabled = false;
+boolean redPressed = false;
+boolean greenPressed = false;
+boolean bluePressed = false;
 
 Rotary r = Rotary(ENCODER_PIN_A, ENCODER_PIN_B);
 Bounce * buttons = new Bounce[NUM_BUTTONS];
@@ -124,6 +130,12 @@ void playBeep() {
     }
 }
 
+void resetColors() {
+    redValue = 0;
+    greenValue = 0;
+    blueValue = 0;
+}
+
 void processButtonInputs() {
     for (int i = 0; i < NUM_BUTTONS; i++) {
         // Update the Bounce instance :
@@ -133,10 +145,13 @@ void processButtonInputs() {
                 case ENCODER_BUTTON_ARRAY_POSITION:
                     break;
                 case RED_SWITCH_ARRAY_POSITION:
+                    redPressed = false;
                     break;
                 case GREEN_SWITCH_ARRAY_POSITION:
+                    greenPressed = false;
                     break;
                 case BLUE_SWITCH_ARRAY_POSITION:
+                    bluePressed = false;
                     break;
                 case FORWARD_SWITCH_ARRAY_POSITION:
                     break;
@@ -159,12 +174,16 @@ void processButtonInputs() {
         if ( buttons[i].rose() ) {
              switch (i) {
                 case ENCODER_BUTTON_ARRAY_POSITION:
+                    resetColors();
                     break;
                 case RED_SWITCH_ARRAY_POSITION:
+                    redPressed = true;
                     break;
                 case GREEN_SWITCH_ARRAY_POSITION:
+                    greenPressed = true;
                     break;
                 case BLUE_SWITCH_ARRAY_POSITION:
+                    bluePressed = true;
                     break;
                 case FORWARD_SWITCH_ARRAY_POSITION:
                     break;
@@ -173,6 +192,31 @@ void processButtonInputs() {
                 case BUZZER_SWITCH_ARRAY_POSITION:
                     Serial.println("Buzzer was disabled.");
                     buzzerEnabled = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if ( buttons[i].read() )
+        {
+            switch (i) {
+                case ENCODER_BUTTON_ARRAY_POSITION:
+                    break;
+                case RED_SWITCH_ARRAY_POSITION:
+                    redPressed = true;
+                    break;
+                case GREEN_SWITCH_ARRAY_POSITION:
+                    greenPressed = true;
+                    break;
+                case BLUE_SWITCH_ARRAY_POSITION:
+                    bluePressed = true;
+                    break;
+                case FORWARD_SWITCH_ARRAY_POSITION:
+                    break;
+                case BACKWARD_SWITCH_ARRAY_POSITION:
+                    break;
+                case BUZZER_SWITCH_ARRAY_POSITION:
                     break;
                 default:
                     break;
@@ -189,10 +233,52 @@ void processClickWheelInputs() {
     else if (result == DIR_CW) {
         Serial.println("ClockWise");
         playBeep();
+        if (redPressed) {
+            if ((redValue >= 0) && (redValue < 255)) {
+                redValue += 1;
+            } else {
+                redValue = 255;
+            }
+        }
+        if (greenPressed) {
+            if ((greenValue >= 0) && (greenValue < 255)) {
+                greenValue += 1;
+            } else {
+                greenValue = 255;
+            }
+        }
+        if (bluePressed) {
+            if ((blueValue >= 0) && (blueValue < 255)) {
+                blueValue += 1;
+            } else {
+                blueValue = 255;
+            }
+        }
     }
     else if (result == DIR_CCW) {
         Serial.println("CounterClockWise");
         playBeep();
+        if (redPressed) {
+            if ((redValue > 0) && (redValue <= 255)) {
+                redValue -= 1;
+            } else {
+                redValue = 0;
+            }
+        }
+        if (greenPressed) {
+            if ((greenValue > 0) && (greenValue <= 255)) {
+                greenValue -= 1;
+            } else {
+                greenValue = 0;
+            }
+        }
+        if (bluePressed) {
+            if ((blueValue > 0) && (blueValue <= 255)) {
+                blueValue -= 1;
+            } else {
+                blueValue = 0;
+            }
+        }
     }
 }
 
@@ -200,6 +286,8 @@ void loop()
 {
     processButtonInputs();
     processClickWheelInputs();  
+
+    fill_solid(leds, NUM_LEDS, CRGB(redValue, greenValue, blueValue));
 
     FastLED.show();
 }
